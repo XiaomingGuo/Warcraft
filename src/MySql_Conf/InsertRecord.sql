9026345a001566d371271804d4e34ea2e5d68d11
xiaomingGuo <guoxiaoming_0@aliyun.com>
2015-05-21 23:44:45 +0800

    Add MySql Config File

---
+++ src/MySql_Conf/InsertRecord.sql
@@ -0,0 +1,15 @@
+-- Create user_info records
+INSERT INTO `earthquake`.`user_info` (`name`,`password`,`department`,`right`) VALUES ("wallace.guo","123456","人事部",1);
+INSERT INTO `earthquake`.`user_info` (`name`,`password`,`department`,`right`) VALUES ('郭晓明', 'abc@123', '生产一部',2);
+INSERT INTO `earthquake`.`product_type` (`name`) VALUES ("文具");
+INSERT INTO `earthquake`.`product_type` (`name`) VALUES ("物料");
+INSERT INTO `earthquake`.`product_type` (`name`) VALUES ("耗材");
+INSERT INTO `earthquake`.`product_type` (`name`) VALUES ("IT附件");
+INSERT INTO `earthquake`.`product_type` (`name`) VALUES ("其他");
+INSERT INTO `earthquake`.`product_info` (`name`,`product_type`) VALUES ("中性笔","文具");
+INSERT INTO `earthquake`.`product_info` (`name`,`product_type`) VALUES ("中性笔芯","文具");
+INSERT INTO `earthquake`.`product_info` (`name`,`product_type`) VALUES ("铁块","物料");
+INSERT INTO `earthquake`.`product_info` (`name`,`product_type`) VALUES ("焊锡","物料");
+INSERT INTO `earthquake`.`material_record` (`proposer`,`material_name`,`QTY`) VALUES ("wallace.guo","中性笔",1,0);
+INSERT INTO `earthquake`.`material_record` (`proposer`,`material_name`,`QTY`) VALUES ("郭晓明","铁块",1,0);
+INSERT INTO `earthquake`.`material_record` (`proposer`,`material_name`,`QTY`) VALUES ("郭晓明","中性笔芯",1,0);
