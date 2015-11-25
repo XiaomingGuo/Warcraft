0512850e01c58fe4f94bce3bd17f1ceecfb4510c
xiaomingGuo <guoxiaoming_0@aliyun.com>
2015-11-25 20:16:06 +0800

    Delete unused code

--- src/com/DB/operation/EarthquakeManagement.java
+++ src/com/DB/operation/EarthquakeManagement.java
@@ -98,32 +98,6 @@ public class EarthquakeManagement implements IEQManagement
 		return rtnRst;
 	}
 	
-	public void getRecord()
-	{
-		try
-		{
-			session = HibernateSessionFactory.getSession();
-			DiscardMaterialRecord tempRecord = (DiscardMaterialRecord)session.get(DiscardMaterialRecord.class, new Integer(1));
-			if (null != tempRecord)
-			{
-				String str1 = tempRecord.getBarCode();
-				String str2 = tempRecord.getBatchLot();
-				String str3 = tempRecord.getOrderName();
-				String str4 = tempRecord.getReason();
-				int str5 = tempRecord.getId();
-				int str6 = tempRecord.getQty();
-			}
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();						//打印异常信息
-		}
-		finally
-		{
-			HibernateSessionFactory.closeSession();
-		}
-	}
-	
 	public void updateRecord(String hql)
 	{
 		try
@@ -143,31 +117,5 @@ public class EarthquakeManagement implements IEQManagement
 		{
 			HibernateSessionFactory.closeSession();
 		}
-	}
-	
-	public List<String> QueryARecord(String hql)
-	{
-		List<String> tempRecord = new ArrayList<String>();
-		try
-		{
-			session = HibernateSessionFactory.getSession();
-			tx=session.beginTransaction();
-			List<DiscardMaterialRecord> tempQ = session.createQuery(hql).list();
-			for(Iterator<DiscardMaterialRecord> idx=tempQ.iterator(); idx.hasNext();)
-			{
-				tempRecord.add(idx.next().getOrderName());
-			}
-			tx.commit();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();						//打印异常信息
-			tx.rollback();
-		}
-		finally
-		{
-			HibernateSessionFactory.closeSession();
-		}
-		return tempRecord;
 	}
 }
