e268716d77f99f3e18cb8c7078cf46588dc4472e
xiaomingGuo <guoxiaoming_0@aliyun.com>
2015-12-17 21:10:33 +0800

    Finish Proxool

---
+++ src/com/Hibernate/Util/ConfigurableProxoolConnectionProvider.java
@@ -0,0 +1,25 @@
+package com.Hibernate.Util;
+
+import java.util.Map;
+import java.util.Properties;
+
+import org.hibernate.service.spi.Configurable;
+import org.hibernate.service.jdbc.connections.internal.ProxoolConnectionProvider;
+
+public class ConfigurableProxoolConnectionProvider extends
+		ProxoolConnectionProvider implements Configurable {
+
+	@Override
+	public void configure(final Map configurationValues) {
+        final Map<?,?> configuration = (Map<?,?>) configurationValues;
+        final Properties properties = new Properties( );
+        for ( final Map.Entry entry : configuration.entrySet() ) {
+            properties.setProperty(
+                String.valueOf( entry.getKey() ),
+                String.valueOf( entry.getValue() ) );
+        }       
+        super.configure( properties );
+
+	}
+
+}
